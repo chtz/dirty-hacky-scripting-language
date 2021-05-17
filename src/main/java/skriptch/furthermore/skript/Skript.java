d2eab2d3e2e9b6066cd991fa0b291a31c05c9c4e
Christian Tschenett <developer@furthermore.ch>
2021-05-17 14:06:16 +0200

    runnable jar

--- src/main/java/skriptch/furthermore/skript/Skript.java
+++ src/main/java/skriptch/furthermore/skript/Skript.java
@@ -1,5 +1,8 @@
 package skriptch.furthermore.skript;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -772,6 +775,27 @@ public class Skript {
 			}
 		}));
 		
+		ctx.put("puts", new FunctionValue(Arrays.asList("s"), new IEvalInContext() {
+			public Value evalInContext(Context global, Context ctx) {
+				String s = ctx.get("s").toString();
+				System.out.println(s);
+				return new IntegerValue(0);
+			}
+		}));
+		
 		return Block.parse(new Tokenizer(s)).eval(new Context(), ctx);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+			for (String line = in.readLine(); line != null; line = in.readLine()) {
+				if (sb.length() > 0) {
+					sb.append("\n");
+				}
+				sb.append(line);
+			}
+		}
+		eval(sb.toString());
 	}
 }
